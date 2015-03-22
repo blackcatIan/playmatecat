00bd32f95ebbed3b2402bf113dda0cf077c8a0f9
blackcatIan <blackcatIan@hotmail.com>
2015-03-23 00:39:49 +0800

    1

--- playmatecat-service/src/main/java/com/playmatecat/cpt/UserCpt.java
+++ playmatecat-service/src/main/java/com/playmatecat/cpt/UserCpt.java
@@ -1,7 +1,10 @@
 package com.playmatecat.cpt;
 
+import java.util.Random;
+
 import net.minidev.json.JSONValue;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +24,9 @@ public class UserCpt {
 				//userService.selectUserById(100000000);
 		Long end = System.currentTimeMillis();
 		//System.out.println("sql finshed in " + (end - start) + " ms");
+
+		user.setUsername(String.valueOf(RandomUtils.nextLong(0, 100000)));
+		
 		
 		return JSONValue.toJSONString(user);
 	}
